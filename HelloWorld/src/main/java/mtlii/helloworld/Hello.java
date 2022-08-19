bd7822a831b4d2d4ac5cdaec3d8b35b9655b4673
Mark Lunetta <lunetta2@msu.edu>
2022-08-19 18:16:13 -0400

    Trying to fix issues with master.

---
+++ HelloWorld/src/main/java/mtlii/helloworld/Hello.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *@author Mark Lunetta, II
+ *email: lunetta2@msu.edu
+ *date: 8/19/22
+ *purpose: My first Hello World program using the NetBeans IDE
+ */
+
+package mtlii.helloworld; // Group ID = Package
+
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Mark! Slow and steady wins the race!");
+    }
+}
